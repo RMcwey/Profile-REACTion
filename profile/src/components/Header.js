129d183861562985a2c19d11cf5c6ac981b6be25
Ross <rmcwey@hotmail.com>
2022-05-20 05:11:16 -0400

    continuing to style and fill out bones of the site.

--- profile/src/components/Header.js
+++ profile/src/components/Header.js
@@ -3,13 +3,7 @@ import React from 'react';
 export default function Header() {
   return (
 <header>
-  <h1 style={{
-          display: 'flex',
-          fontFamily: 'helvetica',
-          flexDirection: 'row',
-          alignItems: 'flex-start',
-          justifyContent: 'flex-start',
-        }}>Ross'React Portfolio</h1>
+  <h1>Ross' React Portfolio</h1>
 </header>
   );
 }
--- profile/src/components/pages/About.js
+++ profile/src/components/pages/About.js
@@ -2,8 +2,9 @@ import React from "react"
 
 export default function About() {
   return (
-  <div>
-    test
+  <div className="about-container">
+    <h2>About Me</h2>
+      <p>This is a test paragraph for me to see the functionality of the page so far!</p>
   </div>
   );
 }

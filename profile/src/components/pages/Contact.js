da33fab8a8f9dcb0af9d5750dc80bb47b375b997
Ross <rmcwey@hotmail.com>
2022-05-21 05:49:37 -0400

    started setting up contact form

--- profile/src/components/pages/Contact.js
+++ profile/src/components/pages/Contact.js
@@ -1,9 +1,31 @@
-import React from "react"
+import React, { useState } from 'react';
 
-export default function Contact() {
+  // First we check to see if "edit" prop exists. If not, we render the normal form
+  // If the prop "edit" exists, we know to render the update form instead
+  export default function Contact () {
   return (
-  <div>
-    test
-  </div>
-  );
+    <div>
+      <form className="contact-form">
+        <input
+          type="text"
+          placeholder="Please enter your name"
+          name="text"
+          className="name-input"
+        ></input>
+        <input
+          type="text"
+          placeholder="Please enter your email"
+          name="text"
+          className="email-input"
+        ></input>
+        <input
+          type="text"
+          placeholder="Please enter your message here"
+          name="text"
+          className="message-input"
+        ></input>
+        <button className="submit-button">Contact Ross</button>
+      </form>
+    </div>
+  ) 
 }

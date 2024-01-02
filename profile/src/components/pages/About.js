e2765ef0b0308826012efe779254dbdb67a894b0
Ross McWey <rossmcwey@gmail.com>
2024-01-02 16:57:26 -0500

    updated about and resume

--- profile/src/components/pages/About.js
+++ profile/src/components/pages/About.js
@@ -42,9 +42,9 @@ export default function About() {
       <div className="profile-text">
         <h2>About Me</h2>
           <p className="about-p-welcome"> Welcome to my portfolio page!</p>
-          <p className="about-p"> I am a curious and passionate full stack developer that really enjoys growing with other people! Ever since I was a child, computers have always been a hobby of mine. One of my favorite things to do was build my own PCs for personal use and tweak programs, games, and other software and hardware for optimization. So, after years of building a successful retail career, thinking that my passion would only ever be a hobby, I decided to pursue programming. When going through GA Tech’s full-stack boot camp, all the creativity, problem-solving, troubleshooting, and joys of success I felt in my hobby came to life in something I could pursue as a career!
+          <p className="about-p"> I am an experienced, curious, and passionate full stack developer that really enjoys growing with other people! Ever since I was a child, computers have always been a hobby of mine. One of my favorite things to do was build my own PCs for personal use and tweak programs, games, and other software and hardware for optimization. So, after years of building a successful retail career, thinking that my passion would only ever be a hobby, I decided to pursue programming. When going through GA Tech’s full-stack boot camp, all the creativity, problem-solving, troubleshooting, and joys of success I felt in my hobby came to life in something I could pursue as a career!
           </p>   
-          <p className="about-p">  In my personal life, I enjoy competitive and difficult games of all types, hiking, playing guitar, singing, song writing, spending time with friends and family, as well as serving as a lay pastor at my local church.</p>
+          <p className="about-p">  In my personal life, I enjoy difficult and competitive games of all types, hiking, playing guitar, singing, song writing, and spending time with friends and family.</p>
       </div>
     </div>
     </div>
